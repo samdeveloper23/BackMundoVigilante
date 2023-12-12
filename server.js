6849255b89ed1219b2466d48078a4ab72e7eb49b
samdeveloper23 <sam.developer.23@gmail.com>
2023-12-12 10:05:12 +0100

    duplicado de escucha eliminado

--- server.js
+++ server.js
@@ -15,7 +15,15 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(fileUpload());
-app.use(cors());
+
+// Configuración CORS
+app.use(cors({
+  origin: 'https://golden-palmier-bb8336.netlify.app',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+}));
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.resolve(process.env.UPLOADS_DIR || 'uploads')));
 
@@ -25,29 +33,26 @@ app.use(userRoutes);
 // MIDDLEWARE PUBLICACIONES
 app.use(publicationRoutes);
 
-//******************//
-
-// const privateKey = fs.readFileSync('/etc/letsencrypt/live/eva00.3utilities.com/privkey.pem', 'utf8');
-// const certificate = fs.readFileSync('/etc/letsencrypt/live/eva00.3utilities.com/fullchain.pem', 'utf8');
-// const credentials = { key: privateKey, cert: certificate };
-
-//******************//
-
 app.use(errorStandard);
 app.use(notFound);
 
-const httpsServer = https.createServer(app);
+const httpsServer = https.createServer({
+  key: fs.readFileSync('/etc/letsencrypt/live/eva00.3utilities.com/privkey.pem', 'utf8'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/eva00.3utilities.com/fullchain.pem', 'utf8'),
+}, app);
 
 httpsServer.listen(process.env.PORT || 443, () => {
-  console.log(`Servidor httpsS escuchando en el puerto ${process.env.httpsS_PORT || 443}`.bgMagenta);
+  console.log(`Servidor HTTPS escuchando en el puerto ${process.env.PORT || 443}`.bgMagenta);
 });
 
-
-
-// https server (optional, to redirect https to httpsS)
+// Opcional: Redirección de HTTP a HTTPS
 const http = require('http');
 const httpsApp = express();
 
 httpsApp.use((req, res) => {
   res.redirect(`https://${req.headers.host}${req.url}`);
+});
+
+http.createServer(httpsApp).listen(80, () => {
+  console.log('Servidor HTTP redirigiendo a HTTPS en el puerto 80');
 });
