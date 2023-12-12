a28c2a99e73e550c0cdec800a11526f8f99e4945
samdeveloper23 <sam.developer.23@gmail.com>
2023-12-12 08:40:09 +0100

    db sincronizada a falta de aplicar seguridad

--- src/db/getDB.js
+++ src/db/getDB.js
@@ -8,22 +8,22 @@ const getDB = async () => {
     try {
         if (!pool) {
             const db = await mysql.createConnection({
-                host: MYSQL_HOST,
-                user: MYSQL_USER,
-                password: MYSQL_PASS,
+                host: 'bfbhminj0t81fyod2xxt-mysql.services.clever-cloud.com',
+                user: 'uozyltbxpvs87x8h',
+                password: 't3XnJ3wu1qRf7XTytpzH',
                 timezone: 'Z',
             });
 
-            db.query(`CREATE DATABASE IF NOT EXISTS ${MYSQL_DB}`);
+            db.query(`CREATE DATABASE IF NOT EXISTS bfbhminj0t81fyod2xxt`);
 
-            db.query(`USE ${MYSQL_DB}`);
+            db.query(`USE bfbhminj0t81fyod2xxt`);
 
             pool = mysql.createPool({
-                connectionLimit: 10,
-                host: MYSQL_HOST,
-                user: MYSQL_USER,
-                password: MYSQL_PASS,
-                database: MYSQL_DB,
+                connectionLimit: 5,
+                host: 'bfbhminj0t81fyod2xxt-mysql.services.clever-cloud.com',
+                user: 'uozyltbxpvs87x8h',
+                password: 't3XnJ3wu1qRf7XTytpzH',
+                database: 'bfbhminj0t81fyod2xxt',
                 timezone: 'Z',
             });
         }
