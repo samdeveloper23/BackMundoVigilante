0d5d88fdc37e839a3338e01091ce169974f0e263
samdeveloper23 <sam.developer.23@gmail.com>
2023-12-13 15:37:32 +0100

    segundo dia de rectificaciones y ajustes0.0

--- src/db/getDB.js
+++ src/db/getDB.js
@@ -8,36 +8,28 @@ const getDB = async () => {
     try {
         if (!pool) {
             const db = await mysql.createConnection({
-                host: 'bfbhminj0t81fyod2xxt-mysql.services.clever-cloud.com',
+                host: 'bkdus5j4s9ph1odfu8z9-mysql.services.clever-cloud.com',
                 user: 'uozyltbxpvs87x8h',
                 password: 't3XnJ3wu1qRf7XTytpzH',
                 timezone: 'Z',
             });
 
-            await db.query(`CREATE DATABASE IF NOT EXISTS bfbhminj0t81fyod2xxt`);
-            await db.query(`USE bfbhminj0t81fyod2xxt`);
+            await db.query(`CREATE DATABASE IF NOT EXISTS bkdus5j4s9ph1odfu8z9`);
+            await db.query(`USE bkdus5j4s9ph1odfu8z9`);
 
             pool = mysql.createPool({
                 connectionLimit: 10,
-                host: 'bfbhminj0t81fyod2xxt-mysql.services.clever-cloud.com',
+                host: 'bkdus5j4s9ph1odfu8z9-mysql.services.clever-cloud.com',
                 user: 'uozyltbxpvs87x8h',
                 password: 't3XnJ3wu1qRf7XTytpzH',
-                database: 'bfbhminj0t81fyod2xxt',
+                database: 'bkdus5j4s9ph1odfu8z9',
                 timezone: 'Z',
             });
         }
 
-        const connection = await pool.getConnection();
-
-        // Código que utiliza la conexión
-
-        // Importante: Cerrar la conexión después de usarla
-        connection.release();
-
-        return connection; // Devolver la conexión, no es necesario obtener otra conexión aquí
+        return await pool.getConnection();
     } catch (error) {
         console.error(error);
-        throw error; // Asegúrate de lanzar el error para que pueda ser manejado externamente
     }
 };
 
